785e1d013458cb60fbe94b87adb83d3160450275
MahmoudRooney <36601941+MahmoudRooney@users.noreply.github.com>
2018-03-03 16:02:44 +0200

    right ratebar

--- app/src/main/java/com/example/egypt2/myapplication/post.java
+++ app/src/main/java/com/example/egypt2/myapplication/post.java
@@ -2,12 +2,22 @@ package com.example.egypt2.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
 
 public class post extends AppCompatActivity {
+    RatingBar rb , ru ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post);
-    }
+
+        rb = (RatingBar) findViewById(R.id.rate_of_book);
+        ru = (RatingBar) findViewById(R.id.rate_of_user);
+
+            }
+
+
 }
